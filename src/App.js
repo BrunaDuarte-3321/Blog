cec4e780c75daecfe548b8a548c306076534a122
Bruna Duarte <62619506+BrunaDuarte-3321@users.noreply.github.com>
2022-02-03 17:57:25 -0300

    Estilizando o menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from './components/Header';
 import Article from './components/Article';
-import Post from './components/Post';
+
 function App() {
   return (
     <>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,36 @@
+import logosvg from '../../svg/logo.svg';
+
 const Header = () => {
-  return <h1>Header.js</h1>;
+  const descLogo = 'Essa é a logo do blog';
+  return (
+    <>
+      <header className="flex-space-between">
+        <div className="logo ">
+          <img src={logosvg} alt={descLogo} />
+        </div>
+        <div className="search">
+          <input type="text" name="search" className="input-search" />
+        </div>
+        <ul className="menu">
+          <li>
+            <a href="#" className="nav-link ">
+              Categoria{' '}
+            </a>
+          </li>
+          <li>
+            <a href="#" className="nav-link ">
+              Sobre{' '}
+            </a>
+          </li>
+          <li>
+            <a href="#" className="nav-link ">
+              Contato
+            </a>
+          </li>
+        </ul>
+      </header>
+    </>
+  );
 };
 
 export default Header;
